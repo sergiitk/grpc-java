8381c9f0ab707af90122b58cc3a01bc9b1f7c60b
Sergii Tkachenko <sergiitk@google.com>
2020-11-26 16:02:25 -0500

    buildscripts: xDS Kubernetes Interop tests buildscript

--- buildscripts/kokoro/xds-k8s.sh
+++ buildscripts/kokoro/xds-k8s.sh
@@ -1,4 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -x
 
 # A placeholder for xDS interop tests executed on GKE
-echo "Coming soon"
+echo "Coming soon. Work in progress"
+
+# Debug info
+if [[ -f /VERSION ]]; then
+  cat /VERSION
+fi
+lsb_release -a
+
+env | grep KOKORO
+
+cd github
+ls -la
