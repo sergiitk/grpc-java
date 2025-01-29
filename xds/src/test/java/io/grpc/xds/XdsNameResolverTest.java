70e6dca2ce68b750df27fd6d7e09393cf4d4ddd9
Sergii Tkachenko <sergiitk@google.com>
2025-01-28 19:21:17 -0800

    XdsNameResolverTest: lenient stubber for fake FaultFilter.Provider

--- xds/src/test/java/io/grpc/xds/XdsNameResolverTest.java
+++ xds/src/test/java/io/grpc/xds/XdsNameResolverTest.java
@@ -27,7 +27,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.reset;
@@ -193,7 +193,10 @@ public class XdsNameResolverTest {
     // Replace FaultFilter.Provider with the one returning FaultFilter injected with mockRandom.
     Filter.Provider faultFilterProvider =
         mock(Filter.Provider.class, delegatesTo(FAULT_FILTER_PROVIDER));
-    doReturn(new FaultFilter(mockRandom, new AtomicLong())).when(faultFilterProvider).newInstance();
+    // Lenient: suppress [MockitoHint] Unused warning, only used in resolved_fault* tests.
+    lenient()
+        .doReturn(new FaultFilter(mockRandom, new AtomicLong()))
+        .when(faultFilterProvider).newInstance();
 
     FilterRegistry filterRegistry = FilterRegistry.newRegistry().register(
         ROUTER_FILTER_PROVIDER,
